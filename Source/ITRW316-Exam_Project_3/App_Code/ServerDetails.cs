391e22cfdd18954ab70914f7d8ede0666a17aa42
Coenraad Human <coen.human@gmail.com>
2019-06-07 10:28:13 +0200

    Changed to OOP

---
+++ Source/ITRW316-Exam_Project_3/App_Code/ServerDetails.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Management;
+
+/// <summary>
+/// Summary description for ServerDetails
+/// </summary>
+public class ServerDetails
+{
+    // system values
+    public long freePhysicalMemory = 0; // KB
+    public long freeVirtualMemory = 0; // KB
+    public long totalVisibleMemorySize = 0; // KB
+    public long totalVirtualMemorySize = 0; // KB
+    public string caption = "";  // display operating system caption
+    public string osArchitecture = ""; // display operating system architecture.
+
+    // results from
+    ManagementObjectCollection results = null;
+    public ServerDetails()
+    {
+    }
+    
+    public void getSystemInformation()
+    {
+        ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher(wql);
+        results = searcher.Get();
+    }
+
+    public void assignRawValues()
+    {
+        foreach (ManagementObject result in results)
+        {
+            // physical memory details
+            freePhysicalMemory = Convert.ToInt64(result["FreePhysicalMemory"]);
+            totalVisibleMemorySize = Convert.ToInt64(result["TotalVisibleMemorySize"]);
+
+            // virtual memory details
+            freeVirtualMemory = Convert.ToInt64(result["FreeVirtualMemory"]);
+            totalVirtualMemorySize = Convert.ToInt64(result["TotalVirtualMemorySize"]);
+
+            // other system details
+            caption = result["Caption"].ToString(); // os name
+            osArchitecture = result["OSArchitecture"].ToString(); // os architecture
+        }
+    }
+}
+
--- Source/ITRW316-Exam_Project_3/Default.aspx.cs
+++ Source/ITRW316-Exam_Project_3/Default.aspx.cs
@@ -9,16 +9,7 @@ using System.Management;   //This namespace is used to work with WMI classes. Fo
 
 public partial class _Default : Page
 {
-    // system values
-    public long freePhysicalMemory = 0; // KB
-    public long freeVirtualMemory = 0; // KB
-    public long totalVisibleMemorySize = 0; // KB
-    public long totalVirtualMemorySize = 0; // KB
-    public string caption = "";  // display operating system caption
-    public string osArchitecture = ""; // display operating system architecture.
-
-    // results from
-    ManagementObjectCollection results = null;
+    ServerDetails server = new ServerDetails();
 
     // user input values
     public long userReservedSize = 0;
@@ -26,34 +17,9 @@ public partial class _Default : Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        getSystemInformation();
-        assignRawValues();
+        server.getSystemInformation();
+        server.assignRawValues();
         updateLabels();
-    }
-
-    public void getSystemInformation()
-    {
-        ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
-        ManagementObjectSearcher searcher = new ManagementObjectSearcher(wql);
-        results = searcher.Get();
-    }
-
-    public void assignRawValues()
-    {
-        foreach (ManagementObject result in results)
-        {
-            // physical memory details
-            freePhysicalMemory = Convert.ToInt64(result["FreePhysicalMemory"]);
-            totalVisibleMemorySize = Convert.ToInt64(result["TotalVisibleMemorySize"]);
-
-            // virtual memory details
-            freeVirtualMemory = Convert.ToInt64(result["FreeVirtualMemory"]);
-            totalVirtualMemorySize = Convert.ToInt64(result["TotalVirtualMemorySize"]);
-
-            // other system details
-            caption = result["Caption"].ToString(); // os name
-            osArchitecture = result["OSArchitecture"].ToString(); // os architecture
-        }
     }
 
     public void updateLabels()
