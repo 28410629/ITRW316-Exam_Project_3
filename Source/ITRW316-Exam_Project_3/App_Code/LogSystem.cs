9758adca9942b3d0125410f69ddc2d8398be4325
Coenraad Human <coen.human@gmail.com>
2019-06-07 12:36:56 +0200

    Simulation updates
    - Added log system class
    - Added program class
    - Initial logic for simulation

---
+++ Source/ITRW316-Exam_Project_3/App_Code/LogSystem.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for LogSystem
+/// </summary>
+public class LogSystem
+{
+    private int totalPrograms = 0;
+    private int totalPageReads = 0;
+    private int totalPageReadsSuccesful = 0;
+    private int totalPageReadsFailed = 0;
+    private int totalPageFaults = 0;
+    private int totalPageFaultsResolved = 0;
+    private int totalUnswappedPages = 0;
+    private int totalHitsTLB = 0;
+    private int totalMissesTLB = 0;
+    private int totalSwappedPages = 0;
+    private int totalPages = 0;
+    private int totalDroppedPages = 0;
+    private double totalPagesFragAmount = 0;
+    private double totalPagesFragSize = 0;
+
+    public LogSystem()
+    {
+    }
+
+    public void logFramentation(int pages, int programSize)
+    {
+        totalPagesFragAmount += (double)pages;
+        totalPagesFragSize += (double)programSize;
+    }
+
+    public void logProgramAdded()
+    {
+        totalPrograms++;
+    }
+
+    public void logPageRead()
+    {
+        totalPageReads++;
+    }
+
+    public void logTLBHit()
+    {
+        totalHitsTLB++;
+    }
+
+    public void logTLBMiss()
+    {
+        totalMissesTLB++;
+    }
+
+    public void logSuccessfulPageRead()
+    {
+        totalPageReadsSuccesful++;
+    }
+
+    public void logFailedPageRead()
+    {
+        totalPageReadsFailed++;
+    }
+
+    public void logPageFaultsResolved()
+    {
+        totalPageFaultsResolved++;
+    }
+
+    public void logPageFaults()
+    {
+        totalPageFaults++;
+    }
+
+    public void logPageDrop()
+    {
+        totalDroppedPages++;
+    }
+
+    public void logPageSwap()
+    {
+        totalSwappedPages++;
+    }
+
+    public void logPageAdd()
+    {
+        totalPages++;
+    }
+
+    public void logPageUnswap()
+    {
+        totalUnswappedPages++;
+    }
+
+
+    public string getLog()
+    {
+        string log = "SESSION LOG DETAILS\n";
+        log += "\nTotal programs added: " + totalPrograms;
+        log += "\nTotal pages: " + totalPages;
+        log += "\nTotal pages swapped: " + totalSwappedPages;
+        log += "\nTotal pages unswapped: " + totalUnswappedPages;
+        log += "\nTotal pages dropped: " + totalDroppedPages;
+        log += "\nTotal page reads: " + totalPageReads;
+        log += "\nTotal succesful page reads: " + totalPageReadsSuccesful;
+        log += "\nTotal failed page reads: " + totalPageReadsFailed;
+        log += "\nTotal page faults: " + totalPageFaults;
+        log += "\nTotal page faults resolved: " + totalPageFaultsResolved;
+        log += "\nTotal TLB hits: " + totalHitsTLB;
+        log += "\nTotal TLB misses: " + totalMissesTLB;
+        //log += "\nAverage fragmentation per page: " + (totalPagesFragSize / totalPagesFragAmount);
+        log += "\n";
+        return log;
+    }
+}
--- Source/ITRW316-Exam_Project_3/App_Code/Simulation.cs
+++ Source/ITRW316-Exam_Project_3/App_Code/Simulation.cs
@@ -3,15 +3,110 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-/// <summary>
-/// Summary description for Simulation
-/// </summary>
 public class Simulation
 {
-    public Simulation()
+    private List<Program> programs = new List<Program>();
+    private LogSystem log = new LogSystem();
+    private Random _random = new Random();
+    private int programAllowed = 0;
+    private int programCounter = 0;
+    private int physicalAllowed = 0;
+    private int physicalCounter = 0;
+    private int secondaryAllowed = 0;
+    private int secondaryCounter = 0;
+
+    public Simulation(int pageCountPhysical, int pageCountSecondary)
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        secondaryAllowed = pageCountSecondary;
+        physicalAllowed = pageCountPhysical;
+        programAllowed = (int)((pageCountPhysical + pageCountSecondary) * 1.2);
+    }
+
+    public void runSimulation()
+    {
+        if (_random.Next(101) > 20)
+        {
+            read(); // 80% chance to read
+        }
+        else
+        {
+            write(); // 20% chance to write
+        }
+    }
+
+    private void read()
+    {
+        if (programs.Count > 0)
+        {
+            log.logPageRead();
+            readProgram(_random.Next(programs.Count));
+        }
+        else
+        {
+            runSimulation();
+        }
+    }
+
+    private void readProgram(int index)
+    {
+        try
+        {
+            Program program = programs.ElementAt(index);
+
+            // check memory if cant find it is page fault
+            if (program.getMemoryStatus())
+            {
+                log.logSuccessfulPageRead(); // read from memory
+            }
+            else
+            {
+                if (program.getDroppedStatus())
+                {
+                    log.logFailedPageRead(); // unavailable, it was dropped in swap
+                }
+                else
+                {
+                    log.logPageFaults();
+                    // run move to physical
+                    log.logPageFaultsResolved();
+                    log.logSuccessfulPageRead();
+                }
+            }
+          
+        }
+        catch (Exception)
+        {
+        }
+    }
+
+    private void write()
+    {
+        if (programCounter < programAllowed)
+        {
+            // make program
+            Program program = new Program(programCounter, "Program " + programCounter);
+            programCounter++;
+
+            // check if physical has space
+            if (physicalCounter < physicalAllowed)
+            {
+                // add to physical
+            }
+            else
+            {
+                // move first in physical to secondary
+                for (int i = 0; i < programs.Count; i++)
+                {
+                    programs.ElementAt(i);
+                }
+            }
+
+            
+            // run simulation
+        }
+        else
+        {
+           // done
+        }
     }
 }
